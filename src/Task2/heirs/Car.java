b4766a626bf188a2e20691e650bdaad713f2cb5a
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-04 21:50:43 +0300

    HomeWork

--- src/Task2/heirs/Car.java
+++ src/Task2/heirs/Car.java
@@ -1,2 +1,11 @@
-package Task2.heirs;public class Car {
+package Task2.heirs;
+
+import Task2.Vehicle;
+
+public class Car extends Vehicle {
+
+    public Car(int maxSpeed, String type) {
+        super(maxSpeed, type);
+    }
+
 }
