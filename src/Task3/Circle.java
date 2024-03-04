51ceaf1fba94c75e5037b08d0dcc630fcc422973
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-04 21:50:52 +0300

    HomeWork

--- src/Task3/Circle.java
+++ src/Task3/Circle.java
@@ -1,2 +1,15 @@
-package Task3;public class Circle {
+package Task3;
+
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return 2 * 3.14 * radius;
+    }
+
 }
