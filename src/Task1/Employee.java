97cf2213360d83e8279b88d587ba9dbe8163b409
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-04 21:50:11 +0300

    HomeWork

--- src/Task1/Employee.java
+++ src/Task1/Employee.java
@@ -1,3 +1,5 @@
+package Task1;
+
 import java.util.Date;
 
 // 1) Переписать код в соответствии с Single Responsibility Principle:
@@ -7,22 +9,34 @@ public class Employee { // Работник
     private Date dob;
     private int baseSalary;
 
-    public Employee(String name, Date dob, int baseSalary) { // Конструктор
+    public Employee(String name, Date dob) { // Конструктор
         this.name = name;
         this.dob = dob;
-        this.baseSalary = baseSalary;
     }
 
     public String getEmpInfo() { // Вывод информации
         return "name - " + name + " , dob - " + dob.toString();
     }
 
-    public int calculateNetSalary() { // Установка зарплаты с учетом налогов
-        int tax = (int) (baseSalary * 0.25); // calculate in otherway
-        return baseSalary - tax;
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", dob=" + dob +
+                ", baseSalary=" + baseSalary +
+                '}';
     }
 }
 
-public class Accountant {
-    
-}
