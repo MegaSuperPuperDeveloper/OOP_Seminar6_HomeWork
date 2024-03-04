2f380ddb7fca3e17669bea6caa4b6d52c1e8eb0b
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-04 21:50:29 +0300

    HomeWork

--- src/Task2/Vehicle.java
+++ src/Task2/Vehicle.java
@@ -1,2 +1,16 @@
-package Task2;public class Vehicle {
+package Task2;
+
+public abstract class Vehicle {
+    int maxSpeed;
+    String type;
+    public Vehicle(int maxSpeed, String type) {
+        this.maxSpeed = maxSpeed;
+        this.type = type;
+    }
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+    public String getType() {
+        return this.type;
+    }
 }
