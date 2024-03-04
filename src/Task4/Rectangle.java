1640d3ebe8afc8a2e05122af8215dfa0a0c2eb7a
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-04 21:51:15 +0300

    HomeWork

--- src/Task4/Rectangle.java
+++ src/Task4/Rectangle.java
@@ -1,2 +1,30 @@
-package Task4;public class Rectangle {
+package Task4;
+
+// 4) Переписать код в соответствии с Liskov Substitution Principle:
+public class Rectangle extends Square {
+
+    private int height;
+
+    public Rectangle(int width, int height) {
+        super(width);
+        this.height = height;
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return super.width;
+    }
+
+    public void setWidth(int width) {
+        super.width = width;
+    }
+
 }
