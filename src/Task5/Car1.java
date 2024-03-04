edbdc08e3d75faa950bf7f6555a52f16d2576060
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-04 21:51:33 +0300

    HomeWork

--- src/Task5/Car1.java
+++ src/Task5/Car1.java
@@ -1,2 +1,15 @@
-package Task5;public class Car1 {
+package Task5;
+
+// 5) Переписать код в соответствии с Dependency Inversion Principle:
+public class Car1 {
+    private iEngine engine;
+
+    public Car1(iEngine engine) {
+        this.engine = engine;
+    }
+    public void start() {
+        this.engine.start();
+    }
+
+
 }
