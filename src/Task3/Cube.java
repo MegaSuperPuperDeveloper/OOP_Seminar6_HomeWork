bbc620f3b12b686f00152e5cb9e4b0d4f34d8a8f
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-04 21:51:00 +0300

    HomeWork

--- src/Task3/Cube.java
+++ src/Task3/Cube.java
@@ -1,2 +1,18 @@
-package Task3;public class Cube {
+package Task3;
+
+public class Cube implements Shape {
+    private int edge;
+
+    public Cube(int edge) {
+        this.edge = edge;
+    }
+
+    @Override
+    public double area() {
+        return 6 * edge * edge;
+    }
+
+    public double volume() {
+        return edge * edge * edge;
+    }
 }
