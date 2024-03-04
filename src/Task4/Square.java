1fdcbaef5f962e301c2d8475d30a2e9a39756204
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-04 21:51:19 +0300

    HomeWork

--- src/Task4/Square.java
+++ src/Task4/Square.java
@@ -1,2 +1,15 @@
-package Task4;public class Square {
+package Task4;
+
+public class Square {
+
+    protected int width;
+
+    public Square(int length) {
+        this.width = length;
+    }
+
+    public void setlength(int length) {
+        this.width = length;
+    }
+
 }
